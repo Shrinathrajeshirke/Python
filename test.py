57f2d275bf256152fe474da058029c6b2c69438e
Shrinath Rajeshirke <shrinathrajeshirke@gmail.com>
2025-09-30 00:03:36 +0530

    packages

---
+++ test.py
@@ -0,0 +1,7 @@
+from mypackage import module1
+
+from mypackage.subpackage import module2
+
+print(f"addition of two numbers {module1.addition(1,3)}")
+
+print(f"multiplication of two numbers {module2.multiply(2,3)}")
